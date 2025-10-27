7d99ea2d1241a28347abc85127231d6fef52de7a
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-10-27 22:01:48 +0100

    Add artisan endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,7 +14,41 @@ Route::get('/', function () {
 
 Route::get('/test', function () {
     return [
-        'message' => 'testing app',
+        'message' => 'testing application',
         'date' => now()
     ];
+});
+
+
+
+
+Route::get('/artisan', function () {
+    Artisan::call('inspire');
+    Artisan::call('optimize:clear');
+    Artisan::call('migrate');
+    Artisan::call('db:seed');
+    Artisan::call('optimize');
+    Artisan::call('inspire');
+    return "Artisan commands executed successfully.";
+});
+
+Route::get('/fresh', function () {
+    Artisan::call('inspire');
+    Artisan::call('optimize:clear');
+    Artisan::call('migrate:fresh');
+    Artisan::call('db:seed');
+    Artisan::call('optimize');
+    Artisan::call('inspire');
+    return "Database refreshed successfully.";
+});
+
+
+Route::get('/run', function (Request $request) {
+    if (!$request->filled('query')) {
+        return "No query parameter provided.";
+    }
+    $query = $request->input('query');
+    Artisan::call($query);
+    Artisan::call('inspire');
+    return "command executed";
 });
